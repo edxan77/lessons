e5bcca08669fc95442d7ad9aea07bd8a0a16d71b
edxan77 <edgar.khanamiryan@gmail.com>
2022-01-21 14:08:29 +0400

    js2

--- list.js
+++ list.js
@@ -1,11 +1,20 @@
-let mas = ["click","doubleclick","scroll","rightclick"]
+let mas = ["click","scroll","rightclick","You pressed any button on keyboard", "You opened page","You resized page",]
 
 window.addEventListener("click",function(){
     alert(mas[0])
 })
 window.addEventListener("scroll",function(){
-    alert(mas[2])
+    alert(mas[1])
 })
 window.addEventListener("contextmenu",function(){
+    alert(mas[2])
+})
+window.addEventListener("keypress",function(){
     alert(mas[3])
+})
+window.addEventListener("load",function(){
+  alert(mas[4])
+})
+window.addEventListener("resize",function(){
+    alert(mas[5])
 })
